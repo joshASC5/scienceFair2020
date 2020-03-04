af648eedf09dce1f22cf97037a90590aa4ee6488
joshASC5 <joshfeli613@gmail.com>
2020-03-03 21:07:50 -0500

    Completed index.html file and styling, reorganized style.css, added shadowing to some elements

--- _js/main.js
+++ _js/main.js
@@ -1,1 +1,12 @@
 "use strict";
+/*
+let navBars = document.getElementsByTageName("nav");
+let sticky = navBar.offsetTop;
+window.onscroll = function(){
+    for(let navBar of navBars){
+        if(window.pageYOffset >= sticky)
+        navBar.classList.add("sticky");
+    else
+        navBar.classList.remove("sticky");
+    }
+}
