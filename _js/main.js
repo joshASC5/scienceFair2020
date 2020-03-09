a89f055b84e6db5b155a55ee6407f3bce5a195cb
joshASC5 <joshfeli613@gmail.com>
2020-03-08 20:24:51 -0400

    Ready to publish!

--- _js/main.js
+++ _js/main.js
@@ -1,12 +1,26 @@
 "use strict";
-/*
-let navBars = document.getElementsByTageName("nav");
-let sticky = navBar.offsetTop;
-window.onscroll = function(){
-    for(let navBar of navBars){
-        if(window.pageYOffset >= sticky)
-        navBar.classList.add("sticky");
-    else
-        navBar.classList.remove("sticky");
+
+let tabPanels = document.querySelectorAll(".tabPanels");
+let tabs = document.querySelectorAll(".tabs");
+
+window.onload = function(){
+    for(let i=0; i<tabs.length; i++){
+        tabs[i].classList.add("noshow");
+        tabPanels[i].onclick = function(){
+            toggleTab(tabs[i]);
+            if(tabs[i].classList.contains("noshow"))
+                tabPanels[i].getElementsByClassName("arrows").innerHTML = "&dtrif;";
+            else
+                tabPanels[i].getElementsByClassName("arrows").innerHTML = "&rtrif;";
+        }
+    }
+}
+
+function toggleTab(tab){
+    if(tab.classList.contains("noshow")){
+        tab.classList.remove("noshow");
+    }
+    else{
+        tab.classList.add("noshow");
     }
 }
